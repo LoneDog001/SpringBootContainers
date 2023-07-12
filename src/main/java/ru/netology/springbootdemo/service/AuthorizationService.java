509bfccaec81fdf024c24fa05d5171ef1d3a8f0e
LoneDog001 <pomeshikov-andrey@mail.ru>
2023-07-12 14:18:59 +0400

    First commit

---
+++ src/main/java/ru/netology/springbootdemo/service/AuthorizationService.java
@@ -0,0 +1,32 @@
+package ru.netology.springbootdemo.service;
+
+import org.springframework.stereotype.Service;
+import ru.netology.springbootdemo.exception.InvalidCredentials;
+import ru.netology.springbootdemo.exception.UnauthorizedUser;
+import ru.netology.springbootdemo.model.Authorities;
+import ru.netology.springbootdemo.repository.UserRepository;
+
+import java.util.List;
+@Service
+public class AuthorizationService {
+    UserRepository userRepository;
+
+    public List<Authorities> getAuthorities(String user, String password) {
+        if (isEmpty(user) || isEmpty(password)) {
+            throw new InvalidCredentials("User name or password is empty");
+        }
+        List<Authorities> userAuthorities = userRepository.getUserAuthorities(user, password);
+        if (isEmpty(userAuthorities)) {
+            throw new UnauthorizedUser("Unknown user " + user);
+        }
+        return userAuthorities;
+    }
+
+    private boolean isEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+
+    private boolean isEmpty(List<?> str) {
+        return str == null || str.isEmpty();
+    }
+}
